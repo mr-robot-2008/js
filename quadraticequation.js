b3bebc1eaa671c04dadd4c01ad8c54e635f86aa3
darshdeep38 <115945757+darshdeep38@users.noreply.github.com>
2022-10-22 23:41:33 -0700

    Create quadraticequation.js

---
+++ quadraticequation.js
@@ -0,0 +1,37 @@
+
+let root1, root2;
+
+
+let a = prompt("Enter the first number: ");
+let b = prompt("Enter the second number: ");
+let c = prompt("Enter the third number: ");
+
+
+let discriminant = b * b - 4 * a * c;
+
+
+if (discriminant > 0) {
+    root1 = (-b + Math.sqrt(discriminant)) / (2 * a);
+    root2 = (-b - Math.sqrt(discriminant)) / (2 * a);
+
+    
+    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+}
+
+else if (discriminant == 0) {
+    root1 = root2 = -b / (2 * a);
+
+    
+    console.log(`The roots of quadratic equation are ${root1} and ${root2}`);
+}
+
+
+else {
+    let realPart = (-b / (2 * a)).toFixed(2);
+    let imagPart = (Math.sqrt(-discriminant) / (2 * a)).toFixed(2);
+
+    
+    console.log(
+    `The roots of quadratic equation are ${realPart} + ${imagPart}i and ${realPart} - ${imagPart}i`
+  );
+}
