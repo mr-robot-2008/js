dd1c4cc24bfed331801d10ba1c36aaa994fab9cb
Ayush Vijayant <ayush.vijayant@gmail.com>
2022-10-31 21:12:18 +0530

    delete todo manager

--- todo_manager/db/migrate/20210628071915_add_user_id_to_todos.rb
+++
@@ -1,5 +0,0 @@
-class AddUserIdToTodos < ActiveRecord::Migration[6.1]
-  def change
-    add_column :todos, :user_id, :bigint
-  end
-end
--- todo_manager/db/migrate/20210628072203_add_last_name_to_users.rb
+++
@@ -1,5 +0,0 @@
-class AddLastNameToUsers < ActiveRecord::Migration[6.1]
-  def change
-    add_column :users, :last_name, :string
-  end
-end
