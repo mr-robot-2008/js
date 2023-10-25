528596bdf7d58c83bced32045e6997bbb5e7f32a
dakshrajput100 <115660640+dakshrajput100@users.noreply.github.com>
2023-10-25 10:50:53 +0530

    Create binarysearch.js

---
+++ binarysearch.js
@@ -0,0 +1,29 @@
+function binarySearch(arr, target) {
+    let left = 0;
+    let right = arr.length - 1;
+
+    while (left <= right) {
+        let mid = Math.floor((left + right) / 2);
+
+        if (arr[mid] === target) {
+            return mid; // Target found, return the index
+        } else if (arr[mid] < target) {
+            left = mid + 1; // Target is in the right half
+        } else {
+            right = mid - 1; // Target is in the left half
+        }
+    }
+
+    return -1; // Target is not in the array
+}
+
+// Example usage:
+const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
+const target = 5;
+const result = binarySearch(arr, target);
+
+if (result !== -1) {
+    console.log(`Element ${target} found at index ${result}`);
+} else {
+    console.log(`Element ${target} not found in the array`);
+}
